9b7d00a848f9245879a214bec0c2f394200af08b
Archin0 <paboene25@gmail.com>
2023-10-11 19:25:14 +0700

    Update Patch - Menghitung Pengeluaran Gaji Lembur dalam Perusahaan (Percobaan 2)

---
+++ Jobsheet 7/WhileGaji08.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class WhileGaji08 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int jumlahKaryawan, jlhJamLembur;
+        double gajiLembur=0, totGajiLembur=0;
+        String jabatan;
+
+        System.out.print("Masukkan jumlah karyawan : ");
+        jumlahKaryawan = scan.nextInt();
+
+        int i = 0;
+        while (i < jumlahKaryawan) {
+            System.out.println("Pilihan jabatan - Direktur, Manajer, Staf");
+            System.out.print("Masukkan jabatan karyawan ke-" + (i+1) + ": ");
+            jabatan = scan.next();
+            System.out.print("Masukkan jumlah jam lembur : ");
+            jlhJamLembur = scan.nextInt();
+            i++;
+
+            if (jabatan.equalsIgnoreCase("direktur")) {
+                continue;
+            } else if (jabatan.equalsIgnoreCase("manajer")) {
+                gajiLembur = jlhJamLembur * 100000;
+            } else if (jabatan.equalsIgnoreCase("staf")) {
+                gajiLembur = jlhJamLembur * 75000;
+            }
+
+            totGajiLembur += gajiLembur;
+            System.out.println("Total gaji lembur : " + totGajiLembur);
+        }
+    }
+}
