877964e82466b2e9fdf6f0d71f5857254eee5fea
Archin0 <paboene25@gmail.com>
2023-10-25 21:28:01 +0700

    Penambahan comment dari Tugas 1

--- Jobsheet 9/Tugas1.java
+++ Jobsheet 9/Tugas1.java
@@ -7,14 +7,17 @@ public class Tugas1 {
         int jlhNilai, max, min, total=0, temp=0;
         double rata2;
 
+        // Input banyaknya elemen 
         System.out.print("Masukkan jumlah mahasiswa : ");
         jlhNilai = sc.nextInt();
 
+        // Input nilai tiap elemen
         for (int i = 0; i < jlhNilai; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilai[i] = sc.nextInt();
         }
 
+        // Mencari nilai tertinggi
         for (int i=0; i < jlhNilai; i++) {
             for (int j=1; j < jlhNilai-i; j++) {
                 if (nilai[j-1] > nilai[j]) {
@@ -25,12 +28,12 @@ public class Tugas1 {
                 }
             }
         }
-
         System.out.print("Nilai tertinggi : ");
         for (int i = jlhNilai - 1; i < jlhNilai; i++) {
             System.out.println(nilai[i]);
         }
 
+        // Mencari nilai terendah
         for (int i=0; i < jlhNilai; i++) {
             for (int j=1; j < jlhNilai-i; j++) {
                 if (nilai[j-1] < nilai[j]) {
@@ -41,16 +44,16 @@ public class Tugas1 {
                 }
             }
         }
-
         System.out.print("Nilai terendah : ");
         for (int i = jlhNilai - 1; i < jlhNilai; i++) {
             System.out.println(nilai[i]);
         }
 
+        // Mencari nilai rata-rata
         for (int i = 0; i < jlhNilai; i++) {
             total += nilai[i];
         }
-
+        
         rata2 = total/jlhNilai;
         System.out.println("Nilai rata-rata : "+rata2);
 
