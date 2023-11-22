d31c18026ea7f237a735d83257bdce51a53692a4
Archin0 <paboene25@gmail.com>
2023-11-22 13:20:23 +0700

    Percobaan 2

--- Jobsheet 13/TerimaKasih_08.java
+++ Jobsheet 13/TerimaKasih_08.java
@@ -1,11 +1,23 @@
 public class TerimaKasih_08 {
-    
+
+    // --using function
     public static void UcapanTerimakasih(){
         System.out.println("Thank you bro.\n"+
         "You inspired me.");
     }
 
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
+
     public static void main(String[] args) {
         UcapanTerimakasih();
+        String ucapan = "Terima Kasih cuy. Panjang-panjang orang baik.";
+        UcapanTambahan(ucapan);
+        
+        //--not using function
+        // System.out.println("Thank you bro.\n"+
+        // "You inspired me.");
     }
 }
+
