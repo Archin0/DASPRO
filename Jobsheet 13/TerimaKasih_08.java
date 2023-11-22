544e90334e43749e93498c3cbcef12421c5322cb
Archin0 <paboene25@gmail.com>
2023-11-22 12:37:17 +0700

    Percobaan 1

---
+++ Jobsheet 13/TerimaKasih_08.java
@@ -0,0 +1,11 @@
+public class TerimaKasih_08 {
+    
+    public static void UcapanTerimakasih(){
+        System.out.println("Thank you bro.\n"+
+        "You inspired me.");
+    }
+
+    public static void main(String[] args) {
+        UcapanTerimakasih();
+    }
+}
